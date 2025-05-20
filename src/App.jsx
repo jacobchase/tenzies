d590753400ffd3daa3d87179d6777360240682f5
jacobchase <jchase996@gmail.com>
2025-05-20 13:52:22 -0500

    added new game functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import Die from "/src/components/Die"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
+import { useWindowSize } from "react-use"
 
 export default function App() {
     
-    const [dice, setDice] = useState(generateAllNewDice())
+    const [dice, setDice] = useState(()=>generateAllNewDice())
+    const { windowWidth, windowHeight } = useWindowSize()
 
+    const gameWon = dice.every(dieObject => dieObject.isHeld) && dice.every(die =>die.value=== dice[0].value)
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
@@ -16,6 +20,9 @@ export default function App() {
             }))
     }
 
+    function newGame(){
+            setDice(generateAllNewDice())
+    }
     function rollDice() {
         setDice(oldDice => oldDice.map(die => {return die.isHeld===true ? die : {...die,value:Math.ceil(Math.random() * 6)}}))
     }
@@ -35,7 +42,8 @@ export default function App() {
             <div className="container">
                 {diceElements}
             </div>
-            <button className="Submit" onClick={rollDice}>Roll</button>
+            <button className="Submit" onClick={gameWon ? newGame : rollDice}>{gameWon ? "New Game!" : "Roll"}</button>
+            {gameWon && <Confetti width={1500} height={700}/>}
         </main>
     )
 }
